5e77782076e1c277d306db29630a281a4cb7d121
Kamil Ozdemir <ozdemirkamil@gmail.com>
2024-01-30 11:27:10 +0100

    17. gün sonu

--- src/test/java/utilities/Driver.java
+++ src/test/java/utilities/Driver.java
@@ -3,6 +3,9 @@ package utilities;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.safari.SafariDriver;
 
 import java.time.Duration;
 
@@ -12,13 +15,52 @@ public class Driver {
         belirlenen browser'a uygun webDriver objesi olusturmak
      */
 
+    private Driver(){
+        // baska class'larin Driver class'indan obje olusturmasini engellemek icin
+        // Singleton pattern kullanilmistir
+        // Singleton pattern class'dan obje olusturulmasini engellemek icin
+        // constructor'i gorunur yapip, erisimini private yapmaya dayanir
+    }
+
     public static WebDriver driver;
 
     public static WebDriver getDriver(){
 
-        WebDriverManager.chromedriver().setup();
+        String browserTercihi = ConfigReader.getProperty("browser");
+        /*
+            Browser'in sadece chrome olmamasi icin
+            configuration.properties'e browser = firefox
+            secenegi ekledik.
+
+            Orada yazan browser tercihini 22.satirda alip
+            tercihe uygun driver olusturmasi icin
+            bir switch statement kullandik
+         */
+
+
         if (driver == null){
-            driver = new ChromeDriver();
+
+            switch (browserTercihi){
+
+                case "firefox" :
+                    WebDriverManager.firefoxdriver().setup();
+                    driver = new FirefoxDriver();
+                    break;
+
+                case "edge" :
+                    WebDriverManager.edgedriver().setup();
+                    driver = new EdgeDriver();
+                    break;
+
+                case "safari" :
+                    WebDriverManager.safaridriver().setup();
+                    driver = new SafariDriver();
+                    break;
+                default:
+                    WebDriverManager.chromedriver().setup();
+                    driver = new ChromeDriver();
+            }
+
             driver.manage().window().maximize();
             driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         }
